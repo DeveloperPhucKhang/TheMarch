0d9ad4f45a5c68c236d7f0e215d11c1f72288bbe
Duy <duypv250592@gmail.com>
2018-06-30 23:34:12 +0700

    fix layout

--- TheMarch/static/home.js
+++ TheMarch/static/home.js
@@ -18,14 +18,14 @@
                     $('#content-slider').hide();
                 } else {
                     var html = '';
-                    for (var i = 0; i < result.list_event_slider.length; i = i+2 ) {
+                    for (var i = 0; i < result.list_event_slider.length; i = i + 2) {
                         if (i == 0) {
                             html += '<div class="item active">';
                         }
                         else {
                             html += '<div class="item">';
                         }
-                        html +=     '<div class="row">' +
+                        html += '<div class="row">' +
                                         '<div class="col-md-6">' +
                                             '<div class="attraction-link">' +
                                                 '<a href="http://www.tablemountain.net/" target="_blank">' +
@@ -75,5 +75,74 @@
 });
 
 function show_error(message) {
- 
+    $.toast({
+        heading: message,
+        text: '',
+        position: 'top-right',
+        loaderBg: '#ff6849',
+        icon: 'error',
+        hideAfter: 3500,
+        stack: 6
+    });
 }
+
+function show_alert(message) {
+    $.toast({
+        heading: message,
+        text: '',
+        position: 'top-right',
+        loaderBg: '#ff6849',
+        icon: 'success',
+        hideAfter: 3500,
+        stack: 6
+    });
+}
+
+function send_mail_contact() {
+    // Load description of event
+    var name = $('#txt_name').val();
+    var mail = $('#txt_mail').val();
+    var phone = $('#txt_phone').val();
+    var address = $('#txt_address').val();
+    var mail_content = $('#txt_mail_content').val();
+    // Validate email
+    var filter = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+    if (mail == "") {
+        show_error('Xin hãy nhập email!');
+        return;
+    }
+    if (!filter.test(mail)) {
+        show_error('Email không đúng!');
+    }
+    if (phone == "") {
+        show_error('Xin hãy nhập số điện thoại!');
+        return;
+    }
+    var data = new FormData();
+    data.append('name', name);
+    data.append('mail', mail);
+    data.append('phone', phone);
+    data.append('address', address);
+    data.append('mail_content', mail_content);
+    $.ajax({
+        url: "/send_mail_contact", //the page containing python script
+        type: "POST", //request type,
+        data: data,
+        cache: false,
+        processData: false,
+        contentType: false,
+        success: function (result) {
+            result = jQuery.parseJSON(result);
+            if (result.result == 'success') {
+                $('.summernote').code(result.description.description);
+            }
+            else {
+                show_error('');
+            }
+        },
+        error: function () {
+            show_error('');
+        },
+    })
+}
+
