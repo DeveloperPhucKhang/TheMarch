5c7fca5779651dd914fdc4e6290b8f2119d747f8
Duy <duypv250592@gmail.com>
2018-06-30 20:34:16 +0700

    bug event

--- TheMarch/static/home-detail-event.js
+++ TheMarch/static/home-detail-event.js
@@ -40,7 +40,7 @@
                 var html = '';
                 for (var i = 0; i < result.list_event_recently.length; i++) {
                     html += '<li>' +
-                                '<div class="thumb"><a href="/home/detail_event/' + result.list_event_recently[i]._id + '"><img src="/' + result.list_event_recently[i].thumbnail + '" alt="' + result.list_event_recently[i].title + '"/></a></div>' +
+                                '<div class="thumb"><a href="/home/detail_event/' + result.list_event_recently[i]._id + '"><img style="max-height: 70px;object-fit: cover;" src="/' + result.list_event_recently[i].thumbnail + '" alt="' + result.list_event_recently[i].title + '"/></a></div>' +
                                 '<div class="w-desk">' +
                                     '<a href="/home/detail_event/' + result.list_event_recently[i]._id + '">' + result.list_event_recently[i].event_type + '</a>' +
                                     '' + result.list_event_recently[i].created_date + ''
