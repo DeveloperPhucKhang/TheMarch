a5167788a0125237ff1837d3d3eb4e0f5e920a34
Duy-Pham-S3 <duy.pham@s3corp.com.vn>
2018-07-02 11:35:24 +0700

    bug event

--- TheMarch/static/home.js
+++ TheMarch/static/home.js
@@ -44,7 +44,7 @@
                                         '</div>' +
                                     '<div class="col-md-6">' +
                                             '<div class="attraction-link">' +
-                                                '<a href="/home/detail_event/' + result.list_event_slider[i]._id + '" target="_blank">' +
+                                                '<a href="/home/detail_event/' + result.list_event_slider[i + 1]._id + '" target="_blank">' +
                                                     '<div class="attraction-img">' +
                                                         '<img style="object-fit: cover;max-height:200px" class="img-responsive img-slider" src="' + result.list_event_slider[i + 1].thumbnail + '" alt="" />' +
                                                         '<div class="attraction-overlay">' +
